91e4e1f819866230ad3e276b25aa6e145926d406
ilyawebdevelop <ilia966@tut.by>
2025-10-04 10:54:42 +0300

    asd

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,14 @@
 import * as flsFunctions from "./modules/functions.js";
 import "./modules/jquery-3.7.1.min.js";
-
+import { Fancybox } from "./modules/fancybox.esm.js";
 import './components.js';
 
 flsFunctions.isWebp();
+
+Fancybox.bind("[data-fancybox]", {
+	closeButton: true,
+});
+
 
 // Import swiper
 import Swiper, { Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar } from 'swiper';
@@ -47,6 +52,32 @@ var mySwiperServices = new Swiper(servicesSlider, {
 		prevEl: servicesSlider?.closest('.sliderW').querySelector('.navArrowPrev'),
 		nextEl: servicesSlider?.closest('.sliderW').querySelector('.navArrowNext'),
 	},
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		768: {
+			slidesPerView: 2,
+		},
+		1400: {
+			slidesPerView: 3,
+		},
+	},
+});
+
+// Инициализация слайдера whySlider
+const whySlider = document.querySelector('.whySlider');
+var mySwiperWhy = new Swiper(whySlider, {
+	slidesPerView: 3,
+	speed: 800,
+	spaceBetween: 30,
+	autoplay: {
+		delay: 3000,
+	},
+	scrollbar: {
+    el: '.whySlider .swiper-scrollbar',
+    draggable: true,
+  },
 	breakpoints: {
 		0: {
 			slidesPerView: 1,
